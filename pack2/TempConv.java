38ce98d9d151d4c12f4fc808b677c59dc5aa659c
shraddha <shraddha@gmail.com>
2018-12-28 19:18:25 +0530

    second commit

--- pack2/TempConv.java
+++ pack2/TempConv.java
@@ -7,9 +7,19 @@ public class TempConv
 
 	public static void main(String[] args) 
 	{
-		System.out.println("enter c value ");
-		int c1=Util1.getInt();
-		Util1.temperatureConversion(c1);	//tempconversion is called from utility pacakage
+		System.out.println("1-->fahre, 2-->cel ");
+		System.out.println("enter your choice ");
+		int choice=Util1.getInt();
+		switch(choice)
+		{
+			case 1: /* convertion from cel to fahre */
+			        Util1.celToFahre();
+			        break;
+			case 2: /* convertion from fahre to cel */
+			        Util1.fahreToCel();
+			        break;
+		}
+		
 	}
 
 }
