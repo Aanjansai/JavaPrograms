8a6bbff7f11c8e3674ed0b5af9ea135867a9ed7d
shraddha <shraddha@gmail.com>
2018-12-28 16:41:23 +0530

    second commit

--- Utility1/Util1.java
+++ Utility1/Util1.java
@@ -25,4 +25,71 @@ public class Util1
 		return floatvalue;
 	}
 	
+	/*TempConversion Method*/
+	
+	public static void temperatureConversion(int c1)
+	{
+		int F=(c1*9/5)+32; //conversion from cel to fehr
+		int C=(F-32)*5/9;  //conversion from fehr to cel
+		System.out.println("f--> "+F);
+		System.out.println("c--> "+C);
+	}
+	
+	/*BubbleSort method*/
+	
+	public static void bubbleSort(int arr[]) //bubble sort method 
+	{
+		int temp;
+		for(int i=1;i<=arr.length-1;i++)
+		{
+			for(int j=0;j<=arr.length-2;j++)
+			{
+			/* use to swap the elements in increment order*/
+				if(arr[j]>arr[j+1])
+				{
+					temp=arr[j];       
+					arr[j]=arr[j+1];    
+					arr[j+1]=temp;
+	
+				}
+			
+		    }
+		}	
+		System.out.println("the sorted array ");
+		for(int k=0;k<=arr.length-1;k++)
+		{
+			System.out.print(arr[k]+" "); //to print the sorted array
+		}
+		
+	}
+	
+	/*InsertionSort method*/
+	
+	  public static void sort( int arr[] )
+	    {
+	        int N = arr.length;
+
+	        int i, j, temp;
+
+	        for (i = 1; i< N; i++) 
+	        {
+	            j = i;
+
+	            temp = arr[i];    
+
+	            while (j > 0 && temp < arr[j-1])
+	            {
+
+	                arr[j] = arr[j-1];
+
+	                j = j-1;
+
+	            }
+	            arr[j] = temp;            
+	        }        
+
+	    }    
+
+
+
 }
