2ba00ef4ce06ea672517b8aab7f338287124ac7d
shraddha <shraddha@gmail.com>
2018-12-31 17:35:26 +0530

    commit

---
+++ pack2/Square.java
@@ -0,0 +1,15 @@
+package pack2;
+
+import Utility1.Util1;
+
+public class Square
+{
+/* main method */
+	public static void main(String[] args) 
+	{
+		System.out.println("enter c value ");
+		double c=Util1.getDouble();
+		Util1.sqrt(c); //calling sqrt method
+	}	
+		
+}
