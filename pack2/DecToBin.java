8b8fafe02032d15eba4891e6605c8f8d12974317
shraddha <shraddha@gmail.com>
2019-01-02 17:44:56 +0530

    commit

---
+++ pack2/DecToBin.java
@@ -0,0 +1,15 @@
+package pack2;
+
+import Utility1.Util1;
+public class DecToBin 
+{
+/* main method */
+	public static void main(String[] args)
+	{
+	
+		System.out.println("enter the decimal number ");
+		int n=Util1.getInt();
+		Util1.decToBin(n);
+	}
+
+}
