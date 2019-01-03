58879c86ce5eec4d8df45060ebe57b48f13a79b8
shraddha <shraddha@gmail.com>
2019-01-03 11:47:43 +0530

    updated

--- Utility1/Util1.java
+++ Utility1/Util1.java
@@ -228,4 +228,36 @@ public class Util1
 		  	return status;
 		}	
 	  /* end of anagram method */
+	  
+	  /* sum of triplets */
+	  public static int threeIntegers(int[] arr)
+		{
+			boolean found=true;
+			for(int i=0;i<arr.length-2;i++)
+			{
+				for(int j=i+1;j<arr.length-1;j++)
+				{
+					for(int k=j+1;k<arr.length;k++)
+					{
+						if(arr[i]+arr[j]+arr[k]==0)
+						{
+							System.out.print(arr[i]);
+							System.out.print(" ");
+							System.out.print(arr[j]);
+							System.out.print(" ");
+							System.out.println(arr[k]);
+							found=true;
+						}
+					}
+				}
+			}
+		
+
+	if(found==false)
+	{
+		System.out.println("not exist ");
+	}
+	return 0;
+	}
+	/* end of method */
 }
